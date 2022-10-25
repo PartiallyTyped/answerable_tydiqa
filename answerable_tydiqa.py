499286ee2d44f879add35a8d28f2d4285decedc0
Quinn Sinclair <quinn.sinclair@pm.me>
2022-10-25 11:23:38 +0300

    preprocessed

---
+++ answerable_tydiqa.py
@@ -0,0 +1,228 @@
+# Copyright 2020 The HuggingFace Datasets Authors and the current dataset script contributor.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# TODO: Address all TODOs and remove all explanatory comments
+"""TODO: Add a description here."""
+
+
+import csv
+import json
+import os
+
+import datasets
+
+
+# TODO: Add BibTeX citation
+# Find for instance the citation on arxiv or on the dataset repo/website
+_CITATION = """\
+@InProceedings{huggingface:dataset,
+title = {A great new dataset},
+author={huggingface, Inc.
+},
+year={2020}
+}
+"""
+
+# TODO: Add description of the dataset here
+# You can copy an official description
+_DESCRIPTION = """\
+This new dataset is designed to solve this great NLP task and is crafted with a lot of care.
+"""
+
+# TODO: Add a link to an official homepage for the dataset here
+_HOMEPAGE = ""
+
+# TODO: Add the licence for the dataset here if you can find it
+_LICENSE = ""
+
+# TODO: Add link to the official dataset URLs here
+# The HuggingFace Datasets library doesn't host the datasets but only points to the original files.
+# This can be an arbitrary nested dict/list of URLs (see below in `_split_generators` method)
+
+class TydiqaBuilderConfig(datasets.BuilderConfig):
+    """BuilderConfig for AnswerableTydiqa"""
+    language:str = "english"
+    monolingual:bool = True
+    def __init__(self, **kwargs):
+        language = kwargs.pop("language", "all")
+        monolingual = kwargs.pop("monolingual", True)
+        super().__init__(**kwargs)
+        self.language = language
+        self.monolingual = monolingual
+
+
+VERSION = datasets.Version("1.1.0")
+RAW="raw"
+PREPROCESSED="preprocessed"
+PREPROC_FOR_CLASSIFICATION = "preprocessed for classification"
+TOKENIZED_FOR_CLASSIFICATION = "tokenized for classification"
+
+class AnswerableTydiqa(datasets.GeneratorBasedBuilder):
+    """TODO: Short description of my dataset."""
+
+
+    # This is an example of a dataset with multiple configurations.
+    # If you don't want/need to define several sub-sets in your dataset,
+    # just remove the BUILDER_CONFIG_CLASS and the BUILDER_CONFIGS attributes.
+
+    # If you need to make complex sub-parts in the datasets with configurable options
+    # You can create your own builder configuration class to store attribute, inheriting from datasets.BuilderConfig
+    BUILDER_CONFIG_CLASS = TydiqaBuilderConfig
+
+    # You will be able to load one or the other configurations in the following list with
+    # data = datasets.load_dataset('my_dataset', 'first_domain')
+    # data = datasets.load_dataset('my_dataset', 'second_domain')
+    # BUILDER_CONFIGS = [
+        # TydiqaBuilderConfig(name=name, version=VERSION)
+        # for language in ["english", "finnish", "japanese"]
+        # for monolingual in [True, False]
+        # for name in [RAW, PREPROCESSED, PREPROC_FOR_CLASSIFICATION, TOKENIZED_FOR_CLASSIFICATION]
+    # ]
+    BUILDER_CONFIGS = [
+        TydiqaBuilderConfig(name="raw", version=VERSION),
+        # TydiqaBuilderConfig(name="preprocessed", version=VERSION),
+    ]
+
+    DEFAULT_CONFIG_NAME = RAW # It's not mandatory to have a default configuration. Just use one if it make sense.
+
+    def _info(self):
+        if self.config.name in (RAW, PREPROCESSED):
+            features = datasets.Features(
+                {
+                    "id": datasets.Value("string"),
+                    "context": datasets.Value("string"),
+                    "question": datasets.Value("string"),
+                    "language": datasets.Value("string"),
+                    "golds": datasets.features.Sequence(
+                        {
+                            "answer_text": datasets.Value("string"),
+                            "answer_start": datasets.Value("int32"),
+                        }
+                    ),
+                }
+            )
+        elif self.config.name == PREPROC_FOR_CLASSIFICATION:
+            features = datasets.Features({
+                "id": datasets.Value("string"),
+                "context": datasets.Value("string"),
+                "question": datasets.Value("string"),
+                "label": datasets.Value("bool")
+            })
+        elif self.config.name == TOKENIZED_FOR_CLASSIFICATION:
+            features = datasets.Features({
+                "id": datasets.Value("string"),
+                "context": datasets.Sequence(datasets.Value("string")),
+                "question": datasets.Sequence(datasets.Value("string")),
+                "label": datasets.Value("bool")
+            })
+        else:
+            raise ValueError("Unknown config name")
+        
+        return datasets.DatasetInfo(
+            # This is the description that will appear on the datasets page.
+            description=_DESCRIPTION,
+            # This defines the different columns of the dataset and their types
+            features=features, # Here we define them above because they are different between the two configurations
+            # If there's a common (input, target) tuple from the features, uncomment supervised_keys line below and
+            # specify them. They'll be used if as_supervised=True in builder.as_dataset.
+            # supervised_keys=("sentence", "label"),
+            # Homepage of the dataset for documentation
+            homepage=_HOMEPAGE,
+            # License for the dataset if available
+            license=_LICENSE,
+            # Citation for the dataset
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager):
+        # TODO: This method is tasked with downloading/extracting the data and defining the splits depending on the configuration
+        # If several configurations are possible (listed in BUILDER_CONFIGS), the configuration selected by the user is in self.config.name
+
+        # dl_manager is a datasets.download.DownloadManager that can be used to download and extract URLS
+        # It can accept any type or nested list/dict and will give back the same structure with the url replaced with path to local files.
+        # By default the archives will be extracted and a path to a cached folder where they are extracted is returned instead of the archive
+        name = {
+            RAW: RAW,
+            PREPROCESSED: PREPROCESSED,
+            PREPROC_FOR_CLASSIFICATION: "preprocessed_for_classification",
+            TOKENIZED_FOR_CLASSIFICATION: "tokenized_for_classification"
+        }[self.config.name]
+        
+        url = "https://raw.githubusercontent.com/PartiallyTyped/answerable_tydiqa/main/{split}/{name}.json"
+        urls = {
+            "train": url.format(split="train", name=name),
+            "validation": url.format(split="validation", name=name),
+        }
+        data_dir = dl_manager.download_and_extract(urls)
+     
+        return [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                # These kwargs will be passed to _generate_examples
+                gen_kwargs={
+                    "filepath": data_dir["train"],
+                    "split": "train",
+                    "language": self.config.language,
+                    "monolingual": self.config.monolingual,
+                },
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.VALIDATION,
+                # These kwargs will be passed to _generate_examples
+                gen_kwargs={
+                    "filepath": data_dir["validation"],
+                    "split": "validation",
+                    "language": self.config.language,
+                    "monolingual": self.config.monolingual,
+                },
+            ),
+        ]
+
+    # method parameters are unpacked from `gen_kwargs` as given in `_split_generators`
+    def _generate_examples(self, filepath, split, language, monolingual):
+        # TODO: This method handles input defined in _split_generators to yield (key, example) tuples from the dataset.
+        # The `key` is for legacy reasons (tfds) and is not important in itself, but must be unique for each example.
+
+        if language == "all":
+            check_language = lambda x: True
+        elif monolingual or split=="train":
+            check_language = language.__eq__
+        else:
+            check_language = lambda x: x != language
+
+        with open(filepath, encoding="utf-8") as f:
+            for key, row in enumerate(f):
+                data = json.loads(row)
+                language = data["language"]
+                
+                if not check_language(language):
+                    continue
+                
+                if self.config.name in (RAW, PREPROCESSED):
+                    yield key, {
+                        "id": data["id"],
+                        "context": data["context"],
+                        "question": data["question"],
+                        "golds": data["golds"],
+                        "language": language,
+                    }
+                elif self.config.name in (PREPROC_FOR_CLASSIFICATION, TOKENIZED_FOR_CLASSIFICATION):
+                    yield key, {
+                        "id": data["id"],
+                        "context": data["context"],
+                        "question": data["question"],
+                        "label": data["label"],
+                    }
+                else:
+                    raise ValueError("Unknown config name")
+
---
+++ build_script.py
@@ -0,0 +1,118 @@
+import datasets as D
+import spacy
+from absl import flags, app
+from spacy.lang.en import English
+from spacy.lang.fi import Finnish
+from spacy.lang.ja import Japanese
+
+from toolz import curry, compose
+import re
+from gensim.parsing.preprocessing import strip_multiple_whitespaces, strip_tags
+from collections import defaultdict
+from xxhash import xxh64_hexdigest
+import pathlib as pl
+
+strip_references = curry(re.sub)(r"\[\d+\]", "")
+strip_quotes = curry(re.sub)(r"['\"]", "")
+strip_double_quotes = curry(re.sub)(r"['\"]{2}", "")
+strip_ellipsis = curry(re.sub)(r"\.{3}", "")
+
+preprocess_fn = compose(
+    strip_multiple_whitespaces,
+    strip_tags,
+    strip_references,
+    strip_quotes,
+    strip_double_quotes,
+    strip_ellipsis,
+)
+
+MODE = flags.DEFINE_enum("mode", "preprocessed", ["preprocessed", "for_classification"], "Which data to build")
+
+def build_preprocessed():
+    processors = {
+        "english": English(),
+        "finnish": Finnish(),
+        "japanese": Japanese(),
+    }
+    for v in processors.values():
+        v.add_pipe("sentencizer")
+
+    raw = D.load_dataset("copenlu/answerable_tydiqa").filter(processors.__contains__, input_columns="language")
+
+    def transform(example):
+        context = preprocess_fn(example["document_plaintext"][0])
+        question = preprocess_fn(example["question_text"][0])
+        golds = example["annotations"][0]
+        language = example["language"][0]
+        answer = preprocess_fn(golds["answer_text"][0])
+        seq_id = xxh64_hexdigest(context + question)
+        
+        nlp = processors[language]
+        answers = list(map(str, nlp(answer).sents))
+        sentences = list(map(str, nlp(context).sents))
+        passes_test = True
+        if bool(answer):
+            passes_test = any(answer in sent for sent in sentences for answer in answers)
+        
+        return {
+            "id": [seq_id],
+            "context": [sentences],
+            "question": [question],
+            "language": [language],
+            "answers": [answers],
+            "passes_test": [passes_test],
+        }
+    
+    def distribute(sample):
+        context = sample["context"][0]
+        question = sample["question"][0]
+        language = sample["language"][0]
+        answers = sample["answers"][0]
+        seq_id = sample["id"][0]
+        out = defaultdict(list)
+        for sent in context:
+            for answer in answers:
+                if answer in sent:
+                    out["id"].append(seq_id)
+                    out["context"].append(sent)
+                    out["question"].append(question)
+                    out["language"].append(language)
+                    out["golds"].append({"answer_text": answer, "answer_start": sent.index(answer)})
+                    break
+            else:
+                out["id"].append(seq_id)
+                out["context"].append(sent)
+                out["question"].append(question)
+                out["language"].append(language)
+                out["golds"].append({"answer_text": "", "answer_start": -1})
+
+        return out
+
+
+
+    out = (
+        raw.map(transform, batched=True, batch_size=1, remove_columns=["document_plaintext", "question_text", "annotations", "language", "document_title", "document_url"])
+        .filter(bool, input_columns="passes_test")
+        .map(distribute, batched=True, batch_size=1, remove_columns=["context", "question", "language", "answers", "passes_test"])
+    )
+
+    for key, value in out.items():
+        path = pl.Path(f"{key}/preprocessed.json")
+        path.parent.mkdir(parents=True, exist_ok=True)
+        value.to_json(path)
+
+    return out
+
+
+
+
+def main(_):
+    if MODE.value == "preprocessed":
+        build_preprocessed()
+    elif MODE.value == "for_classification":
+        raise ValueError("Not implemented yet")
+    
+
+if __name__ == "__main__":
+    flags.mark_flags_as_required(["mode"])
+    app.run(main)
