8179c08bd671059302ada44611b3ddcaa789b2fc
Quinn Sinclair <quinn.sinclair@pm.me>
2022-10-25 11:44:50 +0300

    version bump

--- answerable_tydiqa.py
+++ answerable_tydiqa.py
@@ -65,7 +65,7 @@ class TydiqaBuilderConfig(datasets.BuilderConfig):
         self.task = task
 
 
-VERSION = datasets.Version("1.1.0a")
+VERSION = datasets.Version("1.1.1")
 RAW="raw"
 PREPROCESSED="preprocessed"
 PREPROC_FOR_CLASSIFICATION = "preprocessed for classification"
